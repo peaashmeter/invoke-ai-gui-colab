f8de284c34c9ddf6afbf7e97861b2bec832ce99f
Ivan Yuriev <ivanyr44@gmail.com>
2022-12-13 04:15:23 +0300

    2.2.5 fix

---
+++ globals.py
@@ -0,0 +1,27 @@
+'''
+ldm.invoke.globals defines a small number of global variables that would
+otherwise have to be passed through long and complex call chains.
+
+It defines a Namespace object named "Globals" that contains
+the attributes:
+
+  - root           - the root directory under which "models" and "outputs" can be found
+  - initfile       - path to the initialization file
+  - try_patchmatch - option to globally disable loading of 'patchmatch' module
+'''
+
+import os
+import os.path as osp
+from argparse import Namespace
+
+Globals = Namespace()
+
+# This is usually overwritten by the command line and/or environment variables
+Globals.root = osp.abspath('/root/invokeai')
+
+# Where to look for the initialization file
+Globals.initfile = 'invokeai.init'
+
+# Awkward workaround to disable attempted loading of pypatchmatch
+# which is causing CI tests to error out.
+Globals.try_patchmatch = True
